796e246a62c997cd8b7d6276b64293cd092769a4
LittleHerb <47583636+LittleHerb@users.noreply.github.com>
2019-05-14 14:11:17 +1000

    MovementFullyFunctional

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -8,9 +8,17 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController2D controller;
 
     public float runSpeed = 40f;
+    public float mouseSpeed = 10f;
 
     float horizontalMove = 0f;
     bool Jump = false;
+    Rigidbody2D rigid;
+
+    private void Start()
+    {
+        rigid = this.GetComponent<Rigidbody2D>();
+    }
+
     void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
@@ -19,6 +27,19 @@ public class PlayerMovement : MonoBehaviour
         {
             Jump = true;
         }
+
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            MouseFollow();
+        }
+
+        else
+        {
+            rigid.bodyType = RigidbodyType2D.Dynamic;
+
+        }
+
+
     }
 
     private void FixedUpdate()
@@ -26,5 +47,17 @@ public class PlayerMovement : MonoBehaviour
         //Move our Character and move the same amount regardless of the amount of times called
         controller.Move(horizontalMove * Time.fixedDeltaTime, false, Jump);
         Jump = false;
+    }
+
+    void MouseFollow()
+    {
+        rigid.bodyType = RigidbodyType2D.Static;
+
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 direction = mousePos - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.position = Vector2.MoveTowards(transform.position, mousePos, mouseSpeed * Time.deltaTime);
+
     }
 }
