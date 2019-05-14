8db33e1cbf3cc1f209c94a582123a9d6c61d8f19
LittleHerb <47583636+LittleHerb@users.noreply.github.com>
2019-05-14 15:08:12 +1000

    yahrhbdsLKAHdsf

---
+++ Assets/Scripts/Enemy/EnemyDeath.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDeath : MonoBehaviour
+{
+    public Transform deathPoint;
+    public GameObject player;
+    //public GameObject enemy;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+     
+
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.name == "player")
+        {
+            Destroy(col.gameObject);
+        }  
+   
+    }
+}
