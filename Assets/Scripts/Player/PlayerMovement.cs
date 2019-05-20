825df0d7b950ba33d9e4127182e756dda81360fd
LittleHerb <47583636+LittleHerb@users.noreply.github.com>
2019-05-20 12:01:00 +1000

    stuph

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -16,6 +16,8 @@ public class PlayerMovement : MonoBehaviour
     float horizontalMove = 0f;
     bool Jump = false;
     Rigidbody2D rigid;
+	
+	bool flying = false;
 
     private void Start()
     {
@@ -31,15 +33,16 @@ public class PlayerMovement : MonoBehaviour
             Jump = true;
         }
 
-        if (Input.GetKey(KeyCode.LeftControl))
+        if (Input.GetButton("Fire1"))
         {
+			flying = true;
             MouseFollow();
         }
 
         else
         {
+			flying = false;
             rigid.bodyType = RigidbodyType2D.Dynamic;
-
         }
 
 
@@ -48,6 +51,7 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         //Move our Character and move the same amount regardless of the amount of times called
+		if(!flying)
         controller.Move(horizontalMove * Time.fixedDeltaTime, false, Jump);
         Jump = false;
     }
@@ -57,9 +61,9 @@ public class PlayerMovement : MonoBehaviour
         rigid.bodyType = RigidbodyType2D.Static;
 
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector3 direction = mousePos - transform.position;
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        //USELESS//		Vector3 direction = mousePos - transform.position;		//USELESS//
+        //USELESS//		float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;		//USELESS//
+        //USELESS//		transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);		//USELESS//
         transform.position = Vector2.MoveTowards(transform.position, mousePos, mouseSpeed * Time.deltaTime);
 
     }
