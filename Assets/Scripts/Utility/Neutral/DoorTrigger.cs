2f99fbaf9ed8033786d628ad61b0721dbf3cfd78
LittleHerb <LittleHerb@users.noreply.github.com>
2019-05-29 23:10:22 +1000

    d iiors

---
+++ Assets/Scripts/Utility/Neutral/DoorTrigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+    public SwitchOpenDoor door;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            door.DoorOpens();
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            door.DoorCloses();
+        }
+    }
+
+}
---
+++ Assets/Scripts/Utility/Neutral/SwitchOpenDoor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchOpenDoor : MonoBehaviour
+{
+    Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void DoorOpens()
+    {
+        anim.SetBool("Opens", true);
+
+    }
+
+    public void DoorCloses()
+    {
+        anim.SetBool("Opens", false);
+
+    }
+
+
+
+}
