cf133fcb4e736213855d337de6e296b5ec86abd4
LittleHerb <47583636+LittleHerb@users.noreply.github.com>
2019-06-05 11:37:28 +1000

    adsfasdfasd

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,12 +4,27 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-   
+   #region Variables
     public CharacterController2D controller;
 
+	#region GroundedCheck
+	// For overlap circle to check if player is grounded
+	/*public var top_left = Transform; // Overlap uses positions to make a rectangle, needs two points to make
+	public var bottom_right = Transform;// part of positions for overlap rectangle
+	public var ground_layers = LayerMask; // to check for object layers
+	*/
+	public Transform topLeft;
+	public Transform bottomRight;
+	public LayerMask groundLayers;
+	
+	//var grounded = bool;
+	public bool grounded = false;
+	#endregion
+ 
     public float runSpeed = 40f;
     public float mouseSpeed = 10f;
-	public float timerCountUpModifier = 0.5f;
+	public float groundDistance;
+	public float groundedTime = 0.2f;
 
     private PlayerMovement player;
 
@@ -19,6 +34,7 @@ public class PlayerMovement : MonoBehaviour
 	
 	bool flying = false;
 	public float timer, maxTime;
+	#endregion
     private void Start()
     {
         rigid = this.GetComponent<Rigidbody2D>();
@@ -26,6 +42,8 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
+		isGrounded();
+		
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
         if (Input.GetButtonDown("Jump"))
@@ -51,6 +69,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
+
         //Move our Character and move the same amount regardless of the amount of times called
 		if(!flying)
         controller.Move(horizontalMove * Time.fixedDeltaTime, false, Jump);
@@ -59,12 +78,13 @@ public class PlayerMovement : MonoBehaviour
 
     void MouseFollow()
     {
+		
 		timer += Time.deltaTime;
 		if(timer <= maxTime)
 		{
-			
+		
 			rigid.bodyType = RigidbodyType2D.Kinematic;
-            rigid.velocity = Vector2.zero;
+			rigid.velocity = Vector2.zero;
 
 			Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 			//USELESS//		Vector3 direction = mousePos - transform.position;		//USELESS//
@@ -75,16 +95,18 @@ public class PlayerMovement : MonoBehaviour
 		else
 		{
 			flying = false;
-            rigid.bodyType = RigidbodyType2D.Dynamic;
-			
-			if(timer >= 0f)
-			{
-				timer -= (Time.deltaTime * timerCountUpModifier);
-				
-			}
+			rigid.bodyType = RigidbodyType2D.Dynamic;
 		}
-
+		
+		
     }
+	
+	public void isGrounded()
+	{
+		//Uses overlap variables in variables region to create a overlap around the player to check if grounded
+		grounded = Physics2D.OverlapArea(topLeft.position, bottomRight.position, groundLayers);  
+		
+	}
 
 
 }
